008ad9fd35d670c0cb8fe087ac364ff81e325b3a
harshrajeevsingh <harshrajeevsingh@gmail.com>
2024-03-19 16:54:13 +0530

    route & controller 1st time setup

--- src/app.js
+++ src/app.js
@@ -18,4 +18,12 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// route imports
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+
+// https://localhost:8000/api/v1/users/register
+
 export { app };
