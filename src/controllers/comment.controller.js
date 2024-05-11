8e9c61279769d4b963936f1784ef614c654dd1ff
harshrajeevsingh <harshrajeevsingh@gmail.com>
2024-05-11 23:11:44 +0530

    healthcheck added + comment added

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Video } from "../models/video.models.js";
 import { Comment } from "../models/comment.models.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
@@ -11,7 +12,33 @@ const getVideoComments = asyncHandler(async (req, res) => {
 });
 
 const addComment = asyncHandler(async (req, res) => {
-  // TODO: add a comment to a video
+  const { videoId } = req.params;
+  const { content } = req.body;
+
+  if (!(videoId && content)) {
+    throw new ApiError(400, "videoId or content is not available");
+  }
+
+  const video = await Video.findById(videoId);
+
+  if (video) {
+    throw new ApiError(400, "Video not found");
+  }
+  const comment = await Comment.create({
+    content,
+    video: videoId,
+    owner: req.user?._id,
+  });
+
+  if (!comment) {
+    throw new ApiError(400, "Failed to add comment to DB");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "Comment added successfully"));
+
+  // This need testing rn
 });
 
 const updateComment = asyncHandler(async (req, res) => {
