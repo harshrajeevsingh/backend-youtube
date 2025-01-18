a77cd49e45662f7b05b931745f3d8c343375bed0
harshrajeevsingh <harshrajeevsingh@gmail.com>
2025-01-18 15:11:21 +0530

    fix: added cloudinary to serve on https

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -5,6 +5,7 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_SECRET_KEY,
+  secure: true, // turn off in local dev
 });
 
 const MAX_RETRIES = 3;
